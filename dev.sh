3eb5d3878b88618cbb756c871211f4724be748e3
Nhan Duc Nguyen <ducnhanyb@gmail.com>
2023-10-12 21:46:28 +0900

    Update dev.sh start react

--- dev.sh
+++ dev.sh
@@ -6,11 +6,20 @@ start_rails() {
   bundle check || bundle install --without production
   bundle exec rails db:create
   bundle exec rails db:migrate
-  bundle exec rails s
+  bundle exec rails s -p 3001
+}
+
+start_react() {
+  cd frontend/youtube-sharing
+  npm install
+  npm start &
 }
 
 echo "!!! Starting DB !!!"
 start_db
+
+echo "!!! Starting React !!!"
+start_react
 
 echo "!!! Starting Rails !!!"
 start_rails
