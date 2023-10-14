3079fb731869ce3db55fb2904e3cb58fa58c65a3
Nhan Duc Nguyen <ducnhanyb@gmail.com>
2023-10-14 21:48:34 +0900

    Add video component

---
+++ frontend/youtube-sharing/src/components/Video/Video.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const VideoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 20px;
+  justify-content: center;
+  width: 100%;
+  max-width: 768px;
+  box-shadow: 0 3px 8px rgba(50, 50, 50, 0.17);
+  border: 1px solid #e0e0e0;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const VideoFrame = styled.iframe`
+  width: 70%;
+  height: 100%;
+  min-height: 280px;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Description = styled.div`
+  text-align: left;
+  width: 30%;
+  margin-left: 10px;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+  }
+`;
+
+type VideoProps = {
+  title: string;
+  description: string;
+  url: string;
+  shareBy: string;
+};
+
+const Video = (video: VideoProps) => {
+  return (
+    <VideoContainer>
+      <VideoFrame
+        src={video.url}
+        title={video.title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></VideoFrame>
+      <Description>
+        <h3>{video.title}</h3>
+        <div>Share by: {video.shareBy}</div>
+        <div>Description: {video.description}</div>
+        <div>{video.description}</div>
+      </Description>
+    </VideoContainer>
+  );
+};
+
+export default Video;
--- frontend/youtube-sharing/src/pages/HomePage/HomePage.tsx
+++ frontend/youtube-sharing/src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,44 @@
 import React from "react";
 import { useAuth } from "../../hooks/useAuth";
+import Video from "../../components/Video/Video";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 10px;
+`;
 
 function HomePage() {
+  const videos = [
+    {
+      title: "Funny video",
+      description: "This is a funny video",
+      url: "https://www.youtube.com/embed/Snmh_IJL8M0?si=Htndj-ynHvIPEBH6",
+      shareBy: "admin",
+    },
+    {
+      title: "Funny video",
+      description: "This is a funny video",
+      url: "https://www.youtube.com/embed/1qN72LEQnaU",
+      shareBy: "admin",
+    },
+    {
+      title: "Funny video",
+      description: "This is a funny video",
+      url: "https://www.youtube.com/embed/1qN72LEQnaU",
+      shareBy: "admin",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Home
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      {videos.map((video, index) => (
+        <Video {...video} key={index} />
+      ))}
+    </Container>
   );
 }
 
